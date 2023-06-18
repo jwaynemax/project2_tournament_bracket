89f37ad02d1ff842e02d2daa34f3ac4059b1732a
jm00724 <jm00724@cs6261-vm07.uwg.westga.edu>
2023-06-18 14:07:49 -0400

    brackets tests created and validated.

--- src/app/tournament/brackets/brackets.component.spec.ts
+++ src/app/tournament/brackets/brackets.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { BracketsComponent } from './brackets.component';
 
@@ -21,5 +22,19 @@ describe('BracketsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should veify that the list of winners contains 4 people', () => {
+    component.groupOne = "John";
+    component.groupTwo = "Sally";
+    component.groupThree = "Jane";
+    component.groupFour = "Harry";
+    expect(component.getWinner().length === 4);
+  });
+
+  it('should work with only two groups', () => {
+    component.groupOne = "John";
+    component.groupTwo = "Sally";
+    expect(component.getWinner().length === 2);
   });
 });
