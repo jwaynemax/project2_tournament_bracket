006837b391beba514656831578206466f2e4a9a5
jm00724 <jm00724@cs6261-vm07.uwg.westga.edu>
2023-06-18 19:57:32 -0400

    restet roster-service when error is thrown during registration.

--- src/app/tournament/registration/registration.component.ts
+++ src/app/tournament/registration/registration.component.ts
@@ -14,7 +14,9 @@ export class RegistrationComponent implements OnInit {
   public players: string[] = ['', '', '', '', '', '', '', ''];
   public messages: string;
   public outputPlayers: string[];
-  constructor(private rosterService: RosterService) { }
+  constructor(private rosterService: RosterService) {
+    this.rosterService.contestants = [];
+  }
 
   ngOnInit(): void { }
 
@@ -23,7 +25,6 @@ export class RegistrationComponent implements OnInit {
 
     for (let i = 0; i < this.players.length; i++) {
       let player = this.players[i];
-      console.log(player !== "");
       if (player !== "") {
         try {
           this.rosterService.addContestant(player);
@@ -35,26 +36,11 @@ export class RegistrationComponent implements OnInit {
       }
     }
 
-    // this.players.forEach((value) => {
-    //   if (value != '') {
-    //     try {
-    //       this.rosterService.addContestant(value);
-    //     } catch (error) {
-    //       this.messages = error;
-    //       this.rosterService.contestants = [];
-    //     }
-    //   }
-    // });
-
-    console.log(this.rosterService.getContestants());
-
-
     if (this.messages === '' && this.rosterService.getContestants().length !== 2 && this.rosterService.getContestants().length !== 4 && this.rosterService.getContestants().length !== 8) {
       this.messages = "Must have 2, 4, or 8 Players";
       this.rosterService.contestants = [];
     }
     this.outputPlayers = this.rosterService.getContestants();
-
   }
 
   trackByFn(index: any, item: any) {
